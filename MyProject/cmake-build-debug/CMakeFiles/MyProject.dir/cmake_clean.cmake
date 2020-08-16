70912202032b17bca4c740e42cc023de7d2418dd
Liuxuejun123 <liuxuejun0855@163.com>
2020-08-16 16:14:11 +0800

    day1

---
+++ MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/main.cpp.obj"
+  "MyProject.exe"
+  "MyProject.exe.manifest"
+  "MyProject.pdb"
+  "libMyProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TETRIS/cmake-build-debug/CMakeFiles/TETRIS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TETRIS.dir/main.cpp.obj"
+  "TETRIS.exe"
+  "TETRIS.exe.manifest"
+  "TETRIS.pdb"
+  "libTETRIS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TETRIS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
